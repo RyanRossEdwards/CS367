f465e01c614a32cd890370a79686f0978d548cba
Ryan Edwards <37044278+rryanedwards@users.noreply.github.com>
2019-08-12 14:17:34 +1200

    started Q4

--- A1/einstein.pl
+++ A1/einstein.pl
@@ -1,1 +1,17 @@
-occupant
+%% occupant(house, nat)
+occupant(red, brit).
+occupant(green, X) :- drinkOf(coffee, X).
+occupant(yellow, X) :- smoke(dunhill, X).
+
+petOf(dog, swede).
+petOf(bird, X) :- smoke(pallmall, X).
+
+drinkOf(tea, dane).
+drinkOf(coffee, X) :- occupant(green, X).
+drinkOf(beer, X) :- smoke(bluemaster, X).
+
+smoke(pallmall, X) :- petOf(bird, X).
+smoke(dunhill, X) :- occupant(yellow, X).
+smoke(bluemaster, X) :- drinkOf(beer, X).
+smoke(prince, german).
+
